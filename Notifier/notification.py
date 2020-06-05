6cefb198180709ee840748e39f6cf7d7aca8bcbe
Aidan <aklane.work@gmail.com>
2020-06-05 18:54:21 -0400

    added more notifs and increased efficiency

--- Notifier/notification.py
+++ Notifier/notification.py
@@ -10,26 +10,32 @@ water_message = 'Take a sip of water!'
 stretch_title = 'Stretch Reminder'
 stretch_message = 'Stand up and stretch for a bit!'
 
+break_title = 'Break Reminder'
+break_message = 'Walk away for a few minutes!'
+
 def water():
     while True:
-        time.sleep(600)
+        time.sleep(5)
         notif.notify(water_title, water_message, app_name)
 
 
 def stretch():
     while True:
-        time.sleep(1800)
+        time.sleep(10)
         notif.notify(stretch_title, stretch_message, app_name)
+
+def take_break():
+    while True:
+        time.sleep(60*120)
+        notif.notify(break_title, break_message, app_name)
 
 threads = []
 
-t = threading.Thread(target=water)
-t.start()
-threads.append(t)
-
-t2 = threading.Thread(target=stretch)
-t2.start()
-threads.append(t2)
+reminders = [water, stretch, take_break]
+for x in reminders:
+    temp = threading.Thread(target=x)
+    temp.start()
+    threads.append(temp)
 
 for thread in threads:
     thread.join()
